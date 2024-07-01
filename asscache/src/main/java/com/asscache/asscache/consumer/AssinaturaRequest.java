2dbb7e4b8a836a6c5bd5fd20a91c3cfa4aa259fa
Bruno Rosa Duarte <brunoduartrosa090@gmail.com>
2024-06-30 21:45:55 -0300

    Fazendo o RabbitMQ funcionar corretamente

--- asscache/src/main/java/com/asscache/asscache/consumer/AssinaturaRequest.java
+++ asscache/src/main/java/com/asscache/asscache/consumer/AssinaturaRequest.java
@@ -1,30 +1,24 @@
-//package com.asscache.asscache.consumer;
-//
-//import com.asscache.asscache.producer.PagamentoErroProdutor;
-//import com.asscache.asscache.producer.PagamentoSucessoProdutor;
-//import org.springframework.amqp.rabbit.annotation.RabbitListener;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.messaging.Message;
-//import org.springframework.messaging.handler.annotation.Payload;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.Random;
-//
-//@Component
-//public class AssinaturaRequest {
-//    @Autowired
-//    private PagamentoSucessoProdutor sucessoProdutor;
-//    @Autowired
-//    private PagamentoErroProdutor erroProdutor;
-//
-//    @RabbitListener(queues = {"assinatura-request-queue"})
-//    public void receberMensagem(@Payload Message message){
-//        System.out.println(message);
-//        if (new Random().nextBoolean()){
-//            sucessoProdutor.gerarResposta("Assinatura ativa " + message);
-//        }else {
-//            erroProdutor.gerarResposta("Assinatura inativa " + message);
-//        }
-//    }
-//}
-//
+package com.asscache.asscache.consumer;
+
+import com.asscache.asscache.domain.data.StatusAssinatura;
+import com.asscache.asscache.model.AtualizacaoStatusAssinatura;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class AssinaturaRequest {
+
+    private final StatusAssinatura statusAssinatura;
+
+    public AssinaturaRequest(StatusAssinatura statusAssinatura) {
+        this.statusAssinatura = statusAssinatura;
+    }
+
+    @RabbitListener(queues = "#{rabbitMQConfig.getQueueName()}")
+    public void receberAtualizacaoStatusAssinatura(@Payload AtualizacaoStatusAssinatura atualizacaoStatusAssinatura){
+        statusAssinatura.atualizaStatusAssinatura(atualizacaoStatusAssinatura.codass(), atualizacaoStatusAssinatura.novoFimVigencia());
+    }
+}
+
--- scaa/src/main/java/com/monolitoclean/scaa/controllers/assinaturas/AssinaturaController.java
+++ scaa/src/main/java/com/monolitoclean/scaa/controllers/assinaturas/AssinaturaController.java
@@ -1,15 +1,12 @@
 package com.monolitoclean.scaa.controllers.assinaturas;
 
-import com.monolitoclean.scaa.AssinaturaProxy;
 import com.monolitoclean.scaa.application.dtos.AssinaturaDTO;
 import com.monolitoclean.scaa.application.dtos.CodigosDTO;
 import com.monolitoclean.scaa.application.usecase.assinatura.*;
-import com.monolitoclean.scaa.domain.entities.AssinaturaModel;
 import com.monolitoclean.scaa.repository.AssinaturaMemRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,29 +14,23 @@ import java.util.List;
 @RestController
 @RequestMapping("/servcad")
 public class AssinaturaController {
-    private final AssinaturaMemRepository assinaturaMemRepository;
-    private Logger logger = LoggerFactory.getLogger(AssinaturaController.class);
-    private AssinaturaProxy assinaturaProxy;
     private CadastraAssinaturaUC cadastraAssinatura;
     private AssinaturasPorStatusUC assinaturasPorStatus;
     private ListarAssinaturasDoClienteUC listarAssinaturasDoCliente;
     private ListarAssinaturasDoAppUC listarAssinaturasDoApp;
     private VerificaAssinaturaUC verificaAssinatura;
-    private Environment environment;
 
     @Autowired
     public AssinaturaController(CadastraAssinaturaUC cadastraAssinatura,
                                 AssinaturasPorStatusUC assinaturasPorStatus,
                                 ListarAssinaturasDoClienteUC listarAssinaturasDoCliente,
                                 ListarAssinaturasDoAppUC listarAssinaturasDoApp,
-                                VerificaAssinaturaUC verificaAssinatura, AssinaturaProxy assinaturaProxy, AssinaturaMemRepository assinaturaMemRepository) {
+                                VerificaAssinaturaUC verificaAssinatura) {
         this.cadastraAssinatura = cadastraAssinatura;
         this.assinaturasPorStatus = assinaturasPorStatus;
         this.listarAssinaturasDoCliente = listarAssinaturasDoCliente;
         this.listarAssinaturasDoApp = listarAssinaturasDoApp;
         this.verificaAssinatura = verificaAssinatura;
-        this.assinaturaProxy = assinaturaProxy;
-        this.assinaturaMemRepository = assinaturaMemRepository;
     }
 
     @PostMapping("/assinaturas")
@@ -64,14 +55,6 @@ public class AssinaturaController {
 
     @GetMapping("/invalidaass/{codass}")
     public boolean verificaAssinatura(@PathVariable long codass){
-        logger.info("Verifica assinatura chamado de {}",codass);
-        AssinaturaModel assinaturaModel = assinaturaMemRepository.buscaAssinaturaPorCodigo(codass);
-        if(assinaturaModel == null){
-            System.out.println("codass "+ codass + " not found");
-            return false;
-        }
-
-
-        return assinaturaProxy.verificaAssinatura(codass);
+        return verificaAssinatura.run(codass);
     }
 }
