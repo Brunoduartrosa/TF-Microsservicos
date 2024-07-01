f16bff062a2be68120dde446909145b45ecdb09f
Gabriel Teiga <gabrielteiga99@gmail.com>
2024-06-30 23:42:04 -0300

    Criando varias instancias do cache

--- asscache/src/main/java/com/asscache/asscache/AsscacheApplication.java
+++ asscache/src/main/java/com/asscache/asscache/AsscacheApplication.java
@@ -3,10 +3,12 @@ package com.asscache.asscache;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @EnableRabbit
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AsscacheApplication {
 
